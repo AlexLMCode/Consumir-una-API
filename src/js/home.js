6472538d27901b25244b2943dd9fbd6539eeeb2f
AlexLMCode <alexlujanm@outlook.com>
2020-02-15 12:28:36 -0600

    Creado el template de los elementos de las peliculas

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,6 @@
 console.log('Hola Mundo');
 
-const getUser = new Promise((allfine, allwrong) => {
+/*const getUser = new Promise((allfine, allwrong) => {
     //llamar a un api
     setTimeout(() => {
         allwrong('timeout');
@@ -37,3 +37,63 @@ fetch('https://randomuser.me/api/').then((response) => {
 }).catch(() => {
     console.log('Algo fallo')
 });
+
+//Envolviendo la funcion dentro de los parentesis y poniendolos al final, hace que la funcion se autoejecute
+
+(async function load() {
+
+    async function getData(url) {
+        const response = await fetch(url);
+        return await response.json();
+    }
+
+    const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
+    //const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
+    //const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
+    console.log('pelis-action', actionList);
+
+    function videoItemTemplate(movie) {
+        return (
+            `<div class="primaryPlaylistItem>"
+                <div class="primaryPlaylistItem-image">
+                    <img src="${movie.medium_cover_image}" alt="${movie.title}">
+                </div>
+                <h4 class="primaryPlaylist-title">${movie.title}</h4>
+            </div>`
+        )
+    }
+
+    actionList.data.movies.forEach((movie) => {
+        //debugger
+        const HTMLString = videoItemTemplate(movie);
+        console.log(HTMLString);
+    });
+
+    //const $home = $('.home') //selector de clase con jQuery
+    const $modal = document.getElementById('modal');
+    const $overlay = document.getElementById('overlay');
+    const $hideModal = document.getElementById('hide-modal');
+
+    const $modalTitle = $modal.querySelector('h1');
+    const $modalDescription = $modal.querySelector('p');
+    const $modalImage = $modal.querySelector('img');
+
+    const $featuringContainer = document.getElementById('featuring');
+    const $form = document.getElementById('form');
+    const $home = document.getElementById('home');
+
+
+    const $actionContainer = document.querySelector('action');
+    const $dramaContainer = document.querySelector('drama');
+    const $animationContainer = document.querySelector('animation');
+
+    /*'<div class="primaryPlaylistItem">' +
+    '<div class="primaryPlaylistItem-image">' +
+    '<img src="src/images/covers/midnight.jpg">' +
+    < /div>
+    '<h4 class="primaryPlaylistItem-title">Titulo de la peli </h4>'
+    < /div>'*/ //ASI se haria con jQuery para crear template
+
+    console.log(videoItemTemplate('src/images/covers/bitcoin.jpg', 'bitcoin'));
+
+})();
