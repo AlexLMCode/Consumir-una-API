7721447934c9cc9da9a5f3d4e5c37af609addaee
AlexLMCode <alexlujanm@outlook.com>
2020-02-16 23:41:17 -0600

    Agregada la animacion del modal, mejorados los comentarios del codigo

--- src/js/home.js
+++ src/js/home.js
@@ -42,32 +42,52 @@ fetch('https://randomuser.me/api/').then((response) => {
 
 (async function load() {
 
+    /*const $modalTitle = $modal.querySelector('h1');
+    const $modalDescription = $modal.querySelector('p');
+    const $modalImage = $modal.querySelector('img');
+    const $featuringContainer = document.getElementById('featuring');*/
+
+    /*MOSTRAR EL MODAL CUANDO LE DAMOS CLICK A UNA PELICULA*/
     //const $home = $('.home') //selector de clase con jQuery
     const $modal = document.getElementById('modal');
     const $overlay = document.getElementById('overlay');
     const $hideModal = document.getElementById('hide-modal');
 
-    const $modalTitle = $modal.querySelector('h1');
-    const $modalDescription = $modal.querySelector('p');
-    const $modalImage = $modal.querySelector('img');
+    function showModal(){
+        $overlay.classList.add('active');
+        $modal.style.animation = 'modalIn .8s forwards'
+    }
 
-    const $featuringContainer = document.getElementById('featuring');
-    const $form = document.getElementById('form');
-    const $home = document.getElementById('home');
+    function hideModal(){
+        $overlay.classList.remove('active');
+        $modal.style.animation = 'modalOut .8s forwards'
+    }
+
+    $hideModal.addEventListener('click', hideModal);
 
 
+    function addEventClick($element) {
+        $element.addEventListener('click', ()=>{
+            showModal();
+        })
+    }
+
+    /*TRAER LA DATA DEL SERVIDOR*/
     async function getData(url) {
         const response = await fetch(url);
         return await response.json();
     }
 
-    const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
-    const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
-    const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
-    console.log(actionList);
+    /*MOSTRAR CUADRO DE BUSQUEDA*/
+    const $form = document.getElementById('form');
+    const $home = document.getElementById('home');
 
-    console.log('pelis-action', actionList);
+    $form.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        $home.classList.add('search-active')
+    });
 
+    /*RENDERIZAR LAS PELICULAS EN PANTALLA*/
     function videoItemTemplate(movie) {
         return (
             `<div class="primaryPlaylistItem>"
@@ -88,17 +108,21 @@ fetch('https://randomuser.me/api/').then((response) => {
     function renderMovieList(list, $container) {
         list.forEach((movie) => {
             //debugger
-            $container.children[0].remove();
             const HTMLString = videoItemTemplate(movie);
             const movieElement = createTemplate(HTMLString);
             $container.append(movieElement);
-            console.log(HTMLString);
+            addEventClick($container);
         });
+        $container.children[0].remove();
     }
 
-    const $actionContainer = document.getElementById('#action');
-    const $dramaContainer = document.getElementById('#drama');
-    const $animationContainer = document.getElementById('#animation');
+
+    const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
+    const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
+    const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
+    const $actionContainer = document.getElementById('action');
+    const $dramaContainer = document.getElementById('drama');
+    const $animationContainer = document.getElementById('animation');
 
     renderMovieList(actionList.data.movies, $actionContainer);
     renderMovieList(dramaList.data.movies, $dramaContainer);
